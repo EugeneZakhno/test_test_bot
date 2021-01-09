e442e68ee37c44ce3e1a97625f891331f3e32407
EugeneZakhno <zohan1234567@gmail.com>
2021-01-09 16:13:43 +0300

    changed class Card

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,12 +1,54 @@
 
 public class Card {
-    String pan;
+    String srcPan;
+    String desPan;
     String expDate;
     String cvc;
     String name;
 
+    public Card(String srcPan, String desPan, String expDate, String cvc, String name) {
+        this.srcPan = srcPan;
+        this.desPan = desPan;
+        this.expDate = expDate;
+        this.cvc = cvc;
+        this.name = name;
+    }
 
+    public String getSrcPan() {
+        return srcPan;
+    }
+    public void setSrcPan(String srcPan) {
+        this.srcPan = srcPan;
+    }
+    public void setDesPan(String srcPan) {
+        this.desPan = desPan;
+    }
+    public String getDesPan() {
+        return desPan;
+    }
 
+    public String getExpDate() {
+        return expDate;
+    }
 
+    public void setExpDate(String expDate) {
+        this.expDate = expDate;
+    }
+
+    public String getCvc() {
+        return cvc;
+    }
+
+    public void setCvc(String cvc) {
+        this.cvc = cvc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
