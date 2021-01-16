893436bd59f1da10d1aa8859669893dc8a9441e5
EugeneZakhno <zohan1234567@gmail.com>
2021-01-16 23:23:50 +0300

    changed Bot

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,4 +1,9 @@
-public class Bot /* extends TelegramLongPollingBot*/ {
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
 
     Card card = new Card();
     // создаём  приватную глобальную переменную,  что-бы было проще брать id из чата
