1f02572288a916853459b6bb7a034a2027bbc7cc
EugeneZakhno <zohan1234567@gmail.com>
2021-01-28 23:51:38 +0300

    added method botConnect();

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,10 +1,13 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 public class Bot extends TelegramLongPollingBot {
 
+    private static final long RECONNECT_PAUSE = 100 ;
     Card card = new Card();
     // создаём  приватную глобальную переменную,  что-бы было проще брать id из чата
     private long chat_id;
@@ -44,6 +47,22 @@ public class Bot extends TelegramLongPollingBot {
         }
 
         return msg;
+    }
+    public void botConnect() {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(this);
+            log.info("TelegramAPI started. Look for messages");
+        } catch (TelegramApiRequestException e) {
+            log.error("Cant Connect. Pause " + RECONNECT_PAUSE / 1000 + "sec and try again. Error: " + e.getMessage());
+            try {
+                Thread.sleep(RECONNECT_PAUSE);
+            } catch (InterruptedException e1) {
+                e1.printStackTrace();
+                return;
+            }
+            botConnect();
+        }
     }
 
 }
