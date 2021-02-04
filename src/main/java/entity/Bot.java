c96055d4d52678ac87fae4793bb474e57900a13b
EugeneZakhno <zohan1234567@gmail.com>
2021-02-04 22:12:36 +0300

    send httpRequest

--- src/main/java/entity/Bot.java
+++ src/main/java/entity/Bot.java
@@ -10,6 +10,9 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+import java.io.IOException;
+import java.net.*;
+
 @AllArgsConstructor
 @NoArgsConstructor
 public class Bot extends TelegramLongPollingBot {
@@ -46,11 +49,22 @@ public class Bot extends TelegramLongPollingBot {
         return "1381210707:AAH68fziyIQE91UlKRQC_I1ftdr-KXTfEoQ";
     }
 
-    public String input(String msg){
+    public String input(String msg) throws ProtocolException, MalformedURLException {
         if (msg.contains("Hi") || msg.contains("Hello") || msg.contains("Привет") || msg.contains("Как дела?")) {
             return "Здравствуйте, хотите совершить перевод? \n Введите номер карты";
         }
         if (msg.contains("3454646645564664") || msg.contains("68416719817981798") || msg.contains("684269898219889") || msg.contains("52987988798719")) {
+
+            URL url = new URL("https://www.example.com/login");
+            URLConnection con = null;
+            try {
+                con = url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection http = (HttpURLConnection)con;
+            http.setRequestMethod("POST"); // PUT is another valid option
+            http.setDoOutput(true);
             return "Ok";
         }
         return msg;
