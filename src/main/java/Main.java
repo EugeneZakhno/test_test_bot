7e2c04d9af62800538a63086e84c30d4f2c05606
EugeneZakhno <zohan1234567@gmail.com>
2021-03-14 21:34:32 +0300

    return back to working

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,18 @@
-import entity.TelegramBot;
+import entity.Bot;
+
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
+
 public class Main {
     public static void main(String[] args) {
 
-        //1.Инициализируем API телеграмма
         ApiContextInitializer.init();
-
-        //2.создаём  объект телеграм бота API
         TelegramBotsApi telegram = new TelegramBotsApi();
-
-        //3. создаем объект самого бота
-        TelegramBot telegramBot = new TelegramBot();
         try {
-            telegram.registerBot(telegramBot);
-        }catch (TelegramApiRequestException e){
+            telegram.registerBot( new Bot());
+        } catch (TelegramApiRequestException e) {
             e.printStackTrace();
         }
     }
